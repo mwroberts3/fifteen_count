bc5238b629a86d7290c5cbe943dc9bdf84ca3ff0
Mike <mwroberts89@gmail.com>
2021-11-19 08:58:41 -0500

    starting to make some headway on steam leaderboard stuff

--- renderer/scripts/retrieve-high-scores.js
+++ renderer/scripts/retrieve-high-scores.js
@@ -1,4 +1,4 @@
-const moment = require('moment');
+let steamInfo = require('../steamworksFiles/steamworksInfo.json')
 
 const scoreDisplay = document.querySelector(".score-display");
 const personalHighscoreContainer = document.querySelector('.high-score-container').childNodes[3];
@@ -6,13 +6,18 @@ const personalHighscoreContainer = document.querySelector('.high-score-container
 let arcadeScoresDisplayed = true;
 let taScoresDisplayed = false;
 
-let highScoresArr = [];
+let personalHighscores = [];
 
 if (localStorage.getItem('highscore')) {
-  highScoresArr = JSON.parse(localStorage.getItem('highscore'));
+  personalHighscores = JSON.parse(localStorage.getItem('highscore'));
 }
 
+fetchArcadeLeaderboardScores();
+
 // Game mode select
+// depending on game mode selected, need to call fetchArcadeLeaderboardScores or fetchTimeAttackLeaderboardScores
+
+
 const highscoreSelect = document.querySelector('.high-score-mode-select');
 
 highscoreSelect.addEventListener('click', e => {
@@ -45,11 +50,11 @@ highscoreSelect.addEventListener('click', e => {
 displayArcadeScores();
 
 function displayTimeAttackScores() {
-  document.querySelector('h3').textContent = 'Time Attack Scores'
+  document.querySelector('h3').textContent = 'Time Attack'
 
-  if (localStorage.getItem('highscore') && highScoresArr[0].timeAttack > 0) {
-    personalHighscoreContainer.children[1].textContent = `${highScoresArr[0]['timeAttack']}`;
-    personalHighscoreContainer.children[2].innerHTML = `<strong>Date:</strong>&nbsp;${highScoresArr[0]['taDate']}`;
+  if (localStorage.getItem('highscore') && personalHighscores[0].timeAttack > 0) {
+    personalHighscoreContainer.children[1].textContent = `${personalHighscores[0]['timeAttack']}`;
+    personalHighscoreContainer.children[2].innerHTML = `<strong>Date:</strong>&nbsp;${personalHighscores[0]['taDate']}`;
     personalHighscoreContainer.children[3].innerHTML = ` `;
     personalHighscoreContainer.children[4].innerHTML = ` `;
     scoreDisplay.innerHTML = ``;
@@ -63,51 +68,28 @@ function displayTimeAttackScores() {
 }
 
 function displayArcadeScores() {
-  document.querySelector('h3').textContent = 'Arcade Scores';
+  document.querySelector('h3').textContent = 'Arcade';
   
   // Check for existing personal highscore
   if (localStorage.getItem('highscore')) {
-    personalHighscoreContainer.childNodes[3].textContent = `${highScoresArr[0]['totalPoints']}`;
-    personalHighscoreContainer.childNodes[5].innerHTML = `<strong>Cards Played:</strong>&nbsp;${highScoresArr[0]['totalCardsPlayed']}`;
-    personalHighscoreContainer.childNodes[7].innerHTML = `<strong>Time:</strong>&nbsp;${highScoresArr[0]['totalSeconds']}`;
-    personalHighscoreContainer.childNodes[9].innerHTML = `<strong>Date:</strong>&nbsp;${highScoresArr[0]['date']}`;
+    personalHighscoreContainer.childNodes[3].textContent = `${personalHighscores[0]['totalPoints']}`;
+    personalHighscoreContainer.childNodes[5].innerHTML = `<strong>Cards Played:</strong>&nbsp;${personalHighscores[0]['totalCardsPlayed']}`;
+    personalHighscoreContainer.childNodes[7].innerHTML = `<strong>Time:</strong>&nbsp;${personalHighscores[0]['totalSeconds']}`;
+    personalHighscoreContainer.childNodes[9].innerHTML = `<strong>Date:</strong>&nbsp;${personalHighscores[0]['date']}`;
   } else {
     personalHighscoreContainer.childNodes[3].textContent = `0`;
     personalHighscoreContainer.childNodes[5].innerHTML = `<strong>Cards Played:</strong>&nbsp;0`;
     personalHighscoreContainer.childNodes[7].innerHTML = `<strong>Time:</strong>&nbsp;0`;
     personalHighscoreContainer.childNodes[9].innerHTML = `<strong>Date:</strong>&nbsp;na`;
   }
+}
+
+async function fetchArcadeLeaderboardScores() {
+  let arcadeHighscores = await fetch(`https://partner.steam-api.com/ISteamLeaderboards/GetLeaderboardsForGame/v2/?key=${steamInfo.key}&appid=${steamInfo.appID}`);
   
-  db.collection("highscores")
-    .where("hidden", "==", false)
-    .orderBy("score", "desc")
-    .get()
-    .then((querySnapshot) => {
-      let scoreRank = 1;
-      querySnapshot.forEach((doc) => {
-        scoreDisplay.innerHTML += `
-        <tr>
-          <td>${scoreRank}</td>
-          <td>
-            <span class="points-display">
-              ${doc.data().score}
-            </span>
-          </td>
-          <td>
-          ${doc.data().name}
-        </td>
-          <td>
-            ${doc.data().cards_played}(${Math.round(doc.data().score / doc.data().cards_played)})
-          </td>
-          <td>
-            ${doc.data().seconds_played}
-          </td>
-          <td>
-            ${moment(doc.data().date.toDate()).format('MMM D YYYY')}
-          </td>
-        </tr>
-        `;
-        scoreRank++;
-      });
-    }).catch(err => console.log(err));
+  arcadeHighscores = await arcadeHighscores.json();
+
+  arcadeHighscores = arcadeHighscores.response.leaderboards[0];
+
+  console.log(arcadeHighscores);
 }
